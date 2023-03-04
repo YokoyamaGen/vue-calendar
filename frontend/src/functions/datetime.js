d6ab3fd0e852a9805dcfac1f37fc49c86f5cc55e
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-03-04 21:14:10 +0900

    見た目を整える

--- frontend/src/functions/datetime.js
+++ frontend/src/functions/datetime.js
@@ -1,4 +1,5 @@
 import { format, addHours } from "date-fns";
+import { ja } from "date-fns/locale";
 
 export const getTimeIntervalList = () => {
   // 15分間隔の時間のリストを返す
@@ -36,4 +37,9 @@ export const isGreaterEndThanStart = (
     const end = new Date(`${endDate} ${endTime}`).getTime();
     return end > start;
   }
+};
+
+export const formatDateToJa = (date) => {
+  // 日付を日本語表記で返す
+  return format(new Date(date), "M月d日(E)", { locale: ja });
 };
